0a70b7063c9ccdff59c0ca9d4f67e47c566d6407
MinyeongHong <agka00161@gmail.com>
2023-05-22 20:52:06 +0900

    user model, rating model, repo, provider 생성

--- lib/common/utils/data_utils.dart
+++ lib/common/utils/data_utils.dart
@@ -1,6 +1,9 @@
 import '../const/data.dart';
 
 class DataUtils{
-  static pathToUrl(String val) => 'http://$ip$val';
+  static String pathToUrl(String val) => 'http://$ip$val';
 
+  static List<String> listPathsToUrls(List<String> paths){
+    return paths.map((e) => pathToUrl(e)).toList();
+  }
 }
